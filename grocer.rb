fa362c67c0da1bdea0dfe4529adf541a6fb9e184
Brad Farberman <bfarberman@gmail.com>
2019-10-03 15:13:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,7 @@ def apply_coupons(cart, coupons)
   original_item = cart[item]
   coupon_num = deal[:num] 
   if cart.include?(item) && cart.include?(couponed_item) 
-    cart_count = original_item
+    cart_count = original_item[:count]
 end
 
 
