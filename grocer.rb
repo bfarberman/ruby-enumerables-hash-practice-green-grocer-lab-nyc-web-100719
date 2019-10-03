6df688f8d4393869eb8532bd776142baa003fa82
Brad Farberman <bfarberman@gmail.com>
2019-10-03 15:28:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,7 +56,7 @@ def apply_clearance(cart)
   cart.map {|item, info|
   if info[:clearance]
     item_price = info[:price]
-    item_discount = (item_price * .2).round(2)
+    item_discount = (item_price * 0.20).round(2)
     item_final = item_price - item_discount
     info[:price] = item_final
   end
