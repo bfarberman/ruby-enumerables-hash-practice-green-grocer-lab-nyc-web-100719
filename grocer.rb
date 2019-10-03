8575e3e9728ff4b2568d71b4014b68c94ae0b2ff
Brad Farberman <bfarberman@gmail.com>
2019-10-03 15:18:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,7 @@ def apply_coupons(cart, coupons)
     updated_count = cart_count - coupon_num
     if updated_count >= 0 
       original_item[:count] = updated_count 
-      cart[couponed_item] = {:price 
+      cart[couponed_item] = {:price => 
 end
 
 
